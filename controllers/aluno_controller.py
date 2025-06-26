6d496d73b38fcce01ca936daf5d7eb538f902f52
costaluizz <gomescostaluizflorencio385@gmail.com>
2025-06-26 11:27:13 -0300

    Controle aluno finalizado

---
+++ controllers/aluno_controller.py
@@ -0,0 +1,23 @@
+from models.aluno_model import Aluno
+
+class AlunoController:
+    def cadastrar_aluno(self,nome,email,senha):
+        aluno = Aluno(nome, email, senha)
+        aluno.salvar_aluno()
+        return aluno
+    
+    def excluir_aluno(self, aluno_id):
+        aluno = Aluno.buscar_por_id(aluno_id)
+        if aluno:
+            aluno.deletar()
+            return True
+        return False
+    
+    def listar_alunos(self):
+        return Aluno.buscar_todos()
+    
+    def atualizar_aluno(self,nome,email,senha,id):
+        aluno = Aluno(nome, email, senha, id)
+        aluno.salvar_aluno()
+        return aluno
+    
