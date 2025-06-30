a38f9c86ad32bbcc9881936b442be18f8a2f5a95
costaluizz <gomescostaluizflorencio385@gmail.com>
2025-06-30 11:34:47 -0300

    Finalizado

--- models/cadastrar_models.py
+++ models/cadastrar_models.py
@@ -12,21 +12,14 @@ class Cadastrar:
         conn = conectar()
         cursor = conn.cursor()
         
-        print(self.tipo)
-        if self.tipo =='aluno':
-                sql = """
-                INSERT INTO alunos (nome, email, senha)
-                VALUES (%s, %s, %s)
-                """
-                valores = (self.nome, self.email, self.senha)
+       
+        sql = """
+        INSERT INTO alunos (nome, email, senha)
+        VALUES (%s, %s, %s)
+        """
+        valores = (self.nome, self.email, self.senha)
 
-        elif self.tipo == 'funcionario':
-                sql = """
-                INSERT INTO funcionario (nome, email, senha)
-                VALUES (%s, %s, %s)
-                """
-                valores = (self.nome, self.email, self.senha)
-
+      
 
         cursor.execute(sql, valores)
         conn.commit()
