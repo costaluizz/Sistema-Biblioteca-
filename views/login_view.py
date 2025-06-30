9ac4bc46b8d0a95dc01dc21dbc619250ac02cb6b
costaluizz <gomescostaluizflorencio385@gmail.com>
2025-06-30 11:08:53 -0300

    Login View Semi Finalizado

--- views/login_view.py
+++ views/login_view.py
@@ -1,27 +1,32 @@
 import tkinter as tk
+from tkinter import messagebox
 
 class LoginView:
 
-    def __init__(self, controller):
+    def _init_(self, controller):
         self.controller = controller
         self.root = controller.root
         self.frame = tk.Frame(self.root)
 
+        self.email_var = tk.StringVar()
+        self.senha_var = tk.StringVar()
+        self.tipo_var = tk.StringVar(value="")
+
         self.root.attributes('-fullscreen', True)
+
     def mostrar(self):
         self.root.title("Tela de Login")
         self.frame.pack(padx=20, pady=20)
-        var_opcao = tk.StringVar(value="")
 
-        tk.Label(self.root, text = "E-mail:").pack(pady=8)
-        entry_email = tk.Entry(self.root, width=25)
-        entry_email.pack(pady=5)
+        tk.Label(self.frame, text="E-mail:").pack(pady=8)
+        tk.Entry(self.frame, textvariable=self.email_var, width=25).pack()
 
-        tk.Label(self.root, text = "Senha:").pack(pady=8)
-        entry_senha = tk.Entry(self.root, show="*", width=25)
-        entry_senha.pack(pady=10)
+        tk.Label(self.frame, text="Senha:").pack(pady=8)
+        tk.Entry(self.frame, textvariable=self.senha_var, show="*", width=25).pack()
 
-        tk.Label(self.root, text = "Escolha uma opção:").pack(pady=10)
+        tk.Label(self.frame, text="Escolha o tipo de usuário:").pack(pady=10)
+        tk.Radiobutton(self.frame, text="Aluno", variable=self.tipo_var, value="aluno").pack()
+        tk.Radiobutton(self.frame, text="Funcionário", variable=self.tipo_var, value="funcionario").pack()
 
-        tk.Radiobutton(self.root, text="Aluno", variable=var_opcao, value="aluno").pack()
-        tk.Radiobutton(self.root, text="Funcionário", variable=var_opcao, value="funcionario").pack()
+        tk.Button(self.frame, text="Login", command=self.controller.verificar_login).pack(pady=30)
+        tk.Button(self.frame, text="Cadastrar").pack(pady=40)
